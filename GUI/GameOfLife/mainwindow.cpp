efd1b647261b4d4e941a85cb5069d5aad487d7db
funartic <d.holzinger93@gmail.com>
2017-11-13 20:35:43 +0100

    Slots added.

--- GUI/GameOfLife/mainwindow.cpp
+++ GUI/GameOfLife/mainwindow.cpp
@@ -19,3 +19,63 @@ MainWindow::~MainWindow()
 }
 
 
+
+void MainWindow::on_btnStart_clicked()
+{
+
+}
+
+void MainWindow::on_btnStop_clicked()
+{
+
+}
+
+void MainWindow::on_btnClear_clicked()
+{
+
+}
+
+void MainWindow::on_comboUniverseSize_currentIndexChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_btnUniverseMode_currentIndexChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_btnCellMode_currentIndexChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_spinMS_valueChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_btnLoad_clicked()
+{
+
+}
+
+void MainWindow::on_btnSave_clicked()
+{
+
+}
+
+void MainWindow::on_btnSelectColor_clicked()
+{
+
+}
+
+void MainWindow::on_btnRandomColor_clicked()
+{
+
+}
+
+void MainWindow::on_btnGo_clicked()
+{
+
+}
